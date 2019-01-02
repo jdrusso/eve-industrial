57f9524ab385eca1764a09960b850349c05e4612
JD Russo <jrusso@udel.edu>
2019-01-01 22:10:12 -0500

    Removed some unnecessary prints and debugging code

--- public/engine.py
+++ public/engine.py
@@ -30,147 +30,65 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Methods', 'GET,OPTIONS,PUT,POST,DELETE')
     return response
 
-@app.route('/set/')
-def set():
-	session['key'] = 'value'
-	session.modified = True
-	print(session)
-	return 'ok'
-
-@app.route('/get/')
-def get():
-	print(session)
-	# print(session['key'])
-	return session.get('key', 'not set')
-
 
 @app.route('/clear')
 def clear():
-	try:
 
-		session.clear()
-		print("\nClearing session" % session.sid)
-		print(session, end = ' ')
-	# print(session.sid)
-		session = {}
-		print(session, end = ' ')
-		print("\nCleared %s" % session.sid)
-	except UnboundLocalError:
-		print("nothing")
-	# print(type(session))
-	# session['item'] = None
-	# session['quantity'] = None
-	# session['multibuy_minerals'] = None
-	# session['multibuy_ore'] = None
-	return "Clear"
+    try:
+        session.clear()
+        print("\nClearing session")
 
+    except UnboundLocalError:
+        pass
 
-def read_analyze(session):
-	session.modified = False
-
-	# TODO: Spawn the shekelator here
-	parent_conn, child_conn = Pipe()
-
-	print("Spawning shekelator")
-	p = Process(target=analyze, args=(session["item"], session["quantity"], True, child_conn))
-	p.start()
-
-	print("Receiving:")
-	print(parent_conn.recv())
-
-	print("Multibuy:")
-	# Get string IO object
-	piped_minerals = parent_conn.recv()
-	piped_minerals.seek(0)
-	session["multibuy_minerals"] = piped_minerals.read()
-	print("mineral multibuy obtained: %s" % session["multibuy_minerals"])
-
-
-	# Get string IO object
-	piped_ore = parent_conn.recv()
-	piped_ore.seek(0)
-	session["multibuy_ore"] = piped_ore.read()
-	print("ore multibuy obtained: %s" % session["multibuy_ore"])
-
-	print("Shekelator finished, %s %d " % (session.sid, id(session)))
-	print("Shekelator: %s" % session)
-	session.modified = True
-
+    return "Clear"
 
 
 @app.route("/post_test", methods=['POST'])
 def show_post():
 
-	session.clear()
-	print("\nStarting with session %s" % id(session))
+    session.clear()
 
-	# session = {"item": None, "quantity": None, "multibuy_minerals": None}
-	# session['item'] = None
-	# session['quantity'] = None
-	# session['multibuy_minerals'] = None
-	# session['multibuy_ore'] = None
+    data = request.json['data']
+    item = data['item']
+    quantity = int(data['quantity'])
 
-	print(request.json)
-	data = request.json['data']
-	item = data['item']
-	quantity = int(data['quantity'])
+    session["item"] = item
+    session["quantity"] = int(quantity)
 
-	session["item"] = item
-	session["quantity"] = int(quantity)
+    response = "Looking up information for {0}x {1}(s)...\n".format(quantity, item)
 
-	response = "Looking up information for {0}x {1}(s)...\n".format(quantity, item)
+    parent_conn, child_conn = Pipe()
 
-	# TODO: Spawn the shekelator here
-	parent_conn, child_conn = Pipe()
+    print("Spawning shekelator")
+    p = Process(target=analyze, args=(session["item"], session["quantity"], True, child_conn))
+    p.start()
 
-	# p = Process(target=read_analyze, args=(session,))
-	# p.start()
-	print("Background process spawned")
+    # Receive the initialization string
+    recvd = parent_conn.recv()
 
-	# session['multibuy_ore'] = 'Ores!!'
-	# session['multibuy_minerals'] = 'Minerals'
-
-	print("\n\n ***** regular process session %s" % session)
-	print("session id was %d *** \n\n" % id(session))
+    # print("Multibuy:")
+    # Get string IO object
+    piped_minerals = parent_conn.recv()
+    piped_minerals.seek(0)
+    session["multibuy_minerals"] = piped_minerals.read()
+    print("Mineral multibuy obtained")
 
 
+    # Get string IO object
+    piped_ore = parent_conn.recv()
+    piped_ore.seek(0)
+    session["multibuy_ore"] = piped_ore.read()
+    print("Ore multibuy obtained")
 
-	parent_conn, child_conn = Pipe()
+    piped_prices = parent_conn.recv()
+    session['mineral_names'], session["materials_used"], session["materials_required"], session["materials_excess"] = piped_prices
+    print("Materiaals obtained")
 
-	print("Spawning shekelator")
-	p = Process(target=analyze, args=(session["item"], session["quantity"], True, child_conn))
-	p.start()
+    print("Shekelator finished")
+    session.modified = True
 
-	print("Receiving:")
-	print(parent_conn.recv())
-
-	print("Multibuy:")
-	# Get string IO object
-	piped_minerals = parent_conn.recv()
-	piped_minerals.seek(0)
-	session["multibuy_minerals"] = piped_minerals.read()
-	print("mineral multibuy obtained: %s" % session["multibuy_minerals"])
-
-
-	# Get string IO object
-	piped_ore = parent_conn.recv()
-	piped_ore.seek(0)
-	session["multibuy_ore"] = piped_ore.read()
-	print("ore multibuy obtained: %s" % session["multibuy_ore"])
-
-	piped_prices = parent_conn.recv()
-	session['mineral_names'], session["materials_used"], session["materials_required"], session["materials_excess"] = piped_prices
-	print(piped_prices)
-	# piped_prices.seek(0)
-	# session["materials"] = piped_prices.read()
-	print("materials obtained: %s" % session["materials_used"])
-
-	print("Shekelator finished, %s %d " % (session.sid, id(session)))
-	print("Shekelator: %s" % session)
-	session.modified = True
-
-	print(session)
-	return response
+    return response
 
 
 
@@ -211,61 +129,15 @@ def multibuy_ore():
 @app.route("/material_table")
 def materials_table():
 
-	# response = "<a class='github' href='https://github.com'><b>GitHub</b></a>"
+    response = list(zip(
+        session['mineral_names'],
+        session['materials_used'],
+        session['materials_required'],
+        session['materials_excess']))
 
-	# response = \
-	# '''<Table  selectable={false}>
-    #   <TableHeader displaySelectAll={false}>
-	# 	    <TableRow>
-    # 			<TableHeaderColumn>Mineral</TableHeaderColumn>
-    # 			<TableHeaderColumn>Used</TableHeaderColumn>
-    # 			<TableHeaderColumn>Required</TableHeaderColumn>
-    # 			<TableHeaderColumn>Excess</TableHeaderColumn>
-  	#     </TableRow>
-    #   </TableHeader>
-    #   <TableBody displayRowCheckbox={false}>
-	#   <TableRow>
-	# 			<TableRowColumn>1</TableRowColumn>
-	# 			<TableRowColumn>1</TableRowColumn>
-	# 			<TableRowColumn>John Smith</TableRowColumn>
-	# 			<TableRowColumn>Employed</TableRowColumn>
-	# </TableRow>
-    #   </TableBody>
-    #   </Table>'''
+    print("Sending")
 
-# 	response = \
-# '''&lt;TableRow&gt;
-# 			&lt;TableRowColumn&gt;1&lt;/TableRowColumn&gt;
-# 			&lt;TableRowColumn&gt;1&lt;/TableRowColumn&gt;
-# 			&lt;TableRowColumn&gt;John Smith&lt;/TableRowColumn&gt;
-# 			&lt;TableRowColumn&gt;Employed&lt;/TableRowColumn&gt;
-# &lt;/TableRow&gt;'''
-
-# 	response = \
-# '''<table>
-# <tr>
-# 	<th>Name</th>
-# 	<th>Favorite Color</th>
-# </tr>
-# <tr>
-# 	<td>Bob</td>
-# 	<td>Yellow</td>
-# </tr>
-# <tr>
-# 	<td>Michelle</td>
-# 	<td>Purple</td>
-# </tr>
-# </table>'''
-
-	response = list(zip(
-		session['mineral_names'],
-		session['materials_used'],
-		session['materials_required'],
-		session['materials_excess']))
-
-	print(response)
-
-	return jsonify(response)
+    return jsonify(response)
 
 @app.route("/build_price")
 def build_price():
